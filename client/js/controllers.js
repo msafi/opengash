32a3f0702580890bc21abe244fa476b01856357f
MK Safi <msafi@msafi.com>
2013-08-23 23:57:19 +0300

    Redirecting to auth url from front-end. not good. now moving to server...

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -6,7 +6,26 @@ opengash.controller("getAuthUrl", ['$scope', function ($scope) {
 
 opengash.controller('mainCtrl', [
 	'$scope',
-	function($scope) {
-		$scope.myStuff = 'my stuffz';
+	'$state',
+	'$cookies',
+	'$window',
+	function($scope, $state, $cookies, $window) {
+		if ($cookies.loggedIn == '1' && typeof $cookies.accessToken !== 'undefined') {
+			// User is logged in and has a fresh access token.
+			if (true) { // Has added GA views
+				// Display views
+			}
+			else {
+				// Prompt user to add views.
+			}
+		}
+		else if ($cookies.loggedIn == '1' && typeof $cookies.accessToken === 'undefined') {
+			// User is logged in, but his access token has expired. Silently redirect to authUrl.
+			console.log('ok');
+		}
+		else {
+			// User is not logged in. Display login page.
+			$state.go('login');
+		}
 	}
 ]);
