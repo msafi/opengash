c394e8c43db6b3a6c781ad304fe65b3184dd1447
MK Safi <msafi@msafi.com>
2013-08-26 15:56:24 +0300

    This is just before deleting the heroic self-invoking recursive function that was supposed to iterate over accounts -> web properties -> profiles of Google Analytics. It was replaced with a single API call.

--- client/js/services.js
+++ client/js/services.js
@@ -10,12 +10,13 @@ opengash.factory('authUrl', [
 ]);
 
 // This service is like a static class that's used to communicate with Google APIs.
-opengash.factory('gaApis' ,[
+opengash.factory('gaApi' ,[
 	'$http',
 	'$cookies',
-	function($http, $cookies) {
-		var gaApis = {};
-		gaApis.call = function(apiUrl, callback) {
+	'$q',
+	function($http, $cookies, $q) {
+		var gaApi = {};
+		gaApi.call = function(apiUrl, callback) {
 			var qs = { access_token: $cookies.accessToken }
 			$http({
 				method: 'GET',
@@ -26,35 +27,69 @@ opengash.factory('gaApis' ,[
 				callback(body);
 			});
 		}
-		return gaApis;
+		gaApi.fetchViews = function(callback) {
+			var url = 'https://www.googleapis.com/analytics/v3/management/accounts/~all/webproperties/~all/profiles';
+			gaApi.call(url,
+				function(json) {
+					var profileIds = [];
+					for (i = 0; i < json.items.length; i++) {
+						profileIds.push(json.items[i].id);
+					}
+					console.log(profileIds);
+				}
+			);
+//			(function iterator(url) {
+//				gaApi.call(url, function (json) {
+//					console.log(json);
+//					for (var i = 0; i < json.items.length; i++) {
+//						switch(json.kind) {
+//							case 'analytics#accounts':
+//								console.log('Accounts: ', json.items[i].id);
+//								iterator(json.items[i].selfLink + '/webproperties');
+//								break;
+//							case 'analytics#webproperties':
+//								console.log('Properties: ', json.items[i].id);
+//								iterator(json.items[i].selfLink + '/profiles');
+//								break;
+//							case 'analytics#profiles':
+//								console.log(json.items[i].id);
+//								break;
+//						}
+//					}
+//				});
+//			})(urlx);
+		}
+
+		return gaApi;
 	}
 ]);
 
 opengash.factory('ogAccount', [
 	'$http',
 	'$cookies',
-	'gaApis',
+	'gaApi',
 	'$q',
-	function ($http, $cookies, gaApis, $q) {
+	function ($http, $cookies, gaApi, $q) {
 		var qs = {csrf: $cookies.csrf}
 		var ogAccount = {};
 
-		ogAccount.getGaViews = function () {
+		ogAccount.getSavedViews = function() {
 			var gaViews = $q.defer();
-			$http({method:'GET', url:'api/gaviews/json', params: qs})
+			$http({method:'GET', url:'api/ga-views/json', params: qs})
 				.success(function (json) {
-					gaViews.resolve(json);
-					// Google Analytics views exist. Return them in a JSON.
-				})
-				.error(function() {
-					gaViews.reject(false);
-				})
+					if (json)
+						gaViews.resolve(json);
+					else
+						gaViews.reject(false);
+				});
+			return gaViews.promise;
+		}
+
+		ogAccount.getAllViews = function() {
+			var gaViews = $q.defer();
+			gaApi.fetchViews();
 			return gaViews.promise;
 		}
 		return ogAccount;
 	}
 ]);
-
-//gaApis.call('https://www.googleapis.com/analytics/v3/management/accounts', function (body) {
-//	console.log(body.items[0].id);
-//});
