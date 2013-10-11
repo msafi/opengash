830158a5fa59778291f5d39398b605babe45c86b
MK Safi <msafi@msafi.com>
2013-10-11 14:10:50 +0300

    Removed the deploy route. Too much headache. I'll just run the deploy script manually from the server. It's a one line command to deploy, anyway

--- test/server/routeHandlers/routeHandlers.spec.js
+++ test/server/routeHandlers/routeHandlers.spec.js
@@ -1,9 +1,4 @@
 describe('routeHandlers', function() {
   var rh = require('./dependencies')
   var handlers = require(rh.base)
-
-  it('should have a deploy method', function(done) {
-    expect(handlers.deploy).to.be.true
-    done()
-  })
 })
