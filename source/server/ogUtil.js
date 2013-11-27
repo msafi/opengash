cea09dedc6097b4f8c7c507e01c5ac7d1da672e9
MK Safi <msafi@msafi.com>
2013-11-27 19:29:59 +0300

    Added movement indicator colors under each cell

--- source/server/ogUtil.js
+++ source/server/ogUtil.js
@@ -47,12 +47,12 @@ module.exports.csrf = function (req, res, next) {
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
-      });
+      })
     }
     res.cookie('csrf', uid(), {maxAge: 631138519494}); // 20 years in milliseconds.
   }
-  next();
-};
+  next()
+}
 
 
 /**
